8ace7b178f705df9bd5b85bb8a0f519b8b3c64fd
Sajal Maurya <52036282+sajalrock@users.noreply.github.com>
2022-10-04 08:42:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-apt update && apt upgrade -y
 apt install git -y
 pip3 install -U pip
 
-git clone https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT.git          
-cd Github_Repo
+git clone https://github.com/sajalrock/Rename-bot.git       
+cd Rename-bot
 pip3 install -U -r requirements.txt
 
 echo "𝙱𝚘𝚝 𝙸𝚜 𝚂𝚝𝚊𝚛𝚝𝚒𝚗𝚐........"
+echo "POWERED BY SAJAL....."
 python3 bot.py
