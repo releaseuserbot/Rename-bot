79e163cb76f01c98f24d3e8b9c58109845192a15
Aman Xd <72649318+releaseuserbot@users.noreply.github.com>
2022-10-06 15:58:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 apt install git -y
 pip3 install -U pip
 
-git clone https://github.com/sajalrock/Rename-bot.git       
+git clone https://github.com/releaseuserbot/Rename-bot.git       
 cd Rename-bot
 pip3 install -U -r requirements.txt
 
 echo "𝙱𝚘𝚝 𝙸𝚜 𝚂𝚝𝚊𝚛𝚝𝚒𝚗𝚐........"
-echo "POWERED BY SAJAL....."
+echo "POWERED BY DailyAllTvShows....."
 python3 bot.py
